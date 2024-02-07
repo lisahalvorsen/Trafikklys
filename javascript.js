147997458997db7c7831af0ef001f0184d662c79
Lisa Halvorsen <lisa.w.halvorsen@gmail.com>
2024-02-07 10:19:07 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,33 @@
+let timer;
+
+function turnRedLightOn() {
+  turnOffTrafficlight();
+  document.getElementById("redLight").style.backgroundColor = "red";
+  timer = setTimeout(turnYellowLightOn, 2000);
+}
+
+function turnYellowLightOn() {
+  turnOffTrafficlight();
+  document.getElementById("yellowLight").style.backgroundColor = "yellow";
+  timer = setTimeout(turnGreenLightOn, 2000);
+}
+
+function turnGreenLightOn() {
+  turnOffTrafficlight();
+  document.getElementById("greenLight").style.backgroundColor = "green";
+  timer = setTimeout(turnRedLightOn, 2000);
+}
+
+function turnOnTrafficlight() {
+  timer = setTimeout(turnRedLightOn, 2000);
+}
+
+function turnOffTrafficlight() {
+  document.getElementById("redLight").style.backgroundColor = "";
+  document.getElementById("yellowLight").style.backgroundColor = "";
+  document.getElementById("greenLight").style.backgroundColor = "";
+}
+
+function turnOffTrafficlightParty() {
+  location.reload();
+}
